e5d0ca4e842f604f8ad69073000d0b3e92c779ab
Tommy Tsang <tsangtakyun@gmail.com>
2026-06-08 17:18:52 +0100

    fix: restore native iOS bundling

--- metro.config.js
+++ metro.config.js
@@ -1,13 +1,33 @@
+const path = require('path');
 const { getDefaultConfig } = require('expo/metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 
 const config = getDefaultConfig(__dirname);
 const escapedRoot = __dirname.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 
+const defaultResolveRequest = config.resolver.resolveRequest;
+
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName.startsWith('@/')) {
+    return context.resolveRequest(
+      context,
+      path.resolve(__dirname, 'src', moduleName.slice(2)),
+      platform
+    );
+  }
+
+  if (defaultResolveRequest) {
+    return defaultResolveRequest(context, moduleName, platform);
+  }
+
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 config.resolver.blockList = exclusionList([
   new RegExp(`${escapedRoot}/node_modules\\.(broken|bad)\\.[^/]+/.*`),
   new RegExp(`${escapedRoot}/ios/build/.*`),
-  new RegExp(`${escapedRoot}/\\.tmp-[^/]+/.*`)
+  new RegExp(`${escapedRoot}/\\.tmp-[^/]+/.*`),
+  new RegExp(`${escapedRoot}/\\.env(?:\\s.*|\\..*)?$`)
 ]);
 
 module.exports = config;
